7e46f2b830704399e12711c73f7e0505367b14b0
Lucas-Burford <57986689+Lucas-Burford@users.noreply.github.com>
2021-04-14 18:41:26 +0100

    Add files via upload

---
+++ GameEntity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace UpdatedEngine2
+{
+    public abstract class GameEntity : Entity // Replaces PongEntity from previous version
+    {
+        private Texture2D mTexture;
+        private Vector2 position;
+        private Rectangle hitBox;
+        private Rectangle wallBox;
+
+        public new Texture2D Texture
+        {
+            get { return mTexture; }
+            set { mTexture = value; }
+        }
+
+        public new Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.AntiqueWhite);
+        }
+
+        public virtual new Rectangle HitBox
+        {
+            // Create rectangle at entity position using its width and height
+            get { hitBox = new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height); return hitBox; }
+            set { hitBox.Location = new Point((int)position.X, (int)position.Y); }
+        }
+
+        public virtual Rectangle WallhitBox
+        {
+            get { wallBox = new Rectangle(); return wallBox; }
+        }
+
+        public override void OnCollide(IEntity entityCol)
+        {
+
+        }
+
+        public override void OnInput()
+        {
+
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace UpdatedEngine2
+{
+    /// <summary>
+    /// Class for a Player object
+    /// </summary>
+    class Player : GameEntity
+    {
+        // Enum for selected gender
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+        public Gender gender;
+
+        private KeyboardState keyboardState;
+
+        private float moveSpeed;
+
+        private Vector2 pos;
+        public Vector2 Pos
+        {
+            get { return pos; }
+            set { pos = value; }
+        }
+
+        int number = 5;
+        int number2 = 10;
+
+        // Create a List of type IEntity to store items the player picks up
+        private List<IEntity> playerInventory = new List<IEntity>();
+
+        Rectangle _hitBox;
+
+        public Player()
+        {
+            moveSpeed = 5;
+
+            pos = new Vector2(315, 350);
+
+            Console.WriteLine(number + number2);
+        }
+
+        /// <summary>
+        /// Update player state
+        /// </summary>
+        public override void Update()
+        {
+            // Capture keyboard state every frame 
+            keyboardState = Keyboard.GetState();
+
+            _hitBox.Location = new Point((int)pos.X, (int)pos.Y);
+
+            //Console.WriteLine("X: " + HitBox.X);
+            //Console.WriteLine("Y: " + HitBox.Y);
+
+            CheckWalls();
+        }
+
+        /// <summary>
+        /// Handle collisions with entities
+        /// </summary>
+        /// <param name="entityCol"></param>
+        public override void OnCollide(IEntity entityCol)
+        {
+            if (entityCol is Artefact)
+            {
+                Console.WriteLine("Hit");
+                AddToInventory(entityCol);
+            }
+
+            if (entityCol is Token)
+            {
+                AddToInventory(entityCol);
+            }
+        }
+
+        // Method for handling input
+        public override void OnInput()
+        {
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                pos.Y -= moveSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                pos.Y += moveSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.A))
+            {
+                pos.X -= moveSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.D))
+            {
+                pos.X += moveSpeed;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Space))
+            {
+                Jump();
+            }
+        }
+
+        private void Jump()
+        {
+            pos.Y -= 10;
+        }
+
+        /// <summary>
+        /// Keep player in bounds of the level
+        /// </summary>
+        private void CheckWalls()
+        {
+            // Do walls
+            if (pos.X <= 0)
+            {
+                pos.X += moveSpeed;
+            }
+
+            if (pos.X >= 550)
+            {
+                pos.X -= moveSpeed;
+            }
+
+            if (pos.Y <= -10)
+            {
+                pos.Y += moveSpeed;
+            }
+
+            if (pos.Y >= 550)
+            {
+                pos.Y -= moveSpeed;
+            }
+        }
+
+        /// <summary>
+        /// Add passed in entity to player's inventory
+        /// </summary>
+        /// <param name="entity"></param>
+        public void AddToInventory(IEntity entity)
+        {
+            // Add the passed through item to the player's inventory
+            playerInventory.Add(entity);
+        }
+    }
+}
